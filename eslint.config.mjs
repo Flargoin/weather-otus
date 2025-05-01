4ba250faed305be32633d804eed139652504bef3
Sergey <flargoin@gmail.com>
2025-05-01 15:03:44 +0300

    added html/css/linters

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import jest from "eslint-plugin-jest";
+import eslintRecommended from "@eslint/js/src/configs/eslint-recommended.js";
+
+export default [
+  eslintRecommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      "prefer-const": "error",
+      "no-unused-vars": "error",
+      "no-console": "error",
+      semi: "error",
+    },
+  },
+  {
+    files: ["src/**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+  },
+];
